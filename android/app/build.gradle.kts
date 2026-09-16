98242fa6f9e6a79129ef36652d4b36b23d69c87f
AshishBeluse <ashish.beluse@yahoo.com>
2026-09-16 15:52:03 +0530

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.sleepableai.sleepableai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.0.3"
+        versionCode = 27
+        versionName = "1.0.4"
         multiDexEnabled = true
 
         ndk {
